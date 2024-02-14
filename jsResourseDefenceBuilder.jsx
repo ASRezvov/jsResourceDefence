f5924da006f5230b83fe433d59a39e51c6144047
asRezvov <asrezvov@ya.ru>
2024-02-14 22:32:32 +0300

    start <javascriprtresourse> protection

---
+++ jsResourseDefenceBuilder.jsx
@@ -0,0 +1,66 @@
+﻿<javascriptresource>
+<name>scriptName</name>
+<menu>filter</menu>
+<about>info about your script</about>
+<category>scriptCathegory</category>
+</javascriptresource>
+
+//#####################################################################################
+//########                                                                     ########
+//########    Paste your <javascriptresource> to the begin of this script      ########
+//########    paste natural N - quantity of lines with <javascriptresource>,   ########
+//########        which you want to protect against change                     ########
+//########        to the last line of code                                     ########
+//########                                                                     ########
+//########    After, save this JSX-script and run it under ESTK.               ########
+//########                                                                     ########
+//########    After, you see an array in ESTK Javascript Console,              ########
+//########        paste it to code in "jsResourseDefenceChecker.jsx"           ########
+//########        & make a function, which return false, if                    ########
+//########        <javascriptresource> changes.                                ########
+//########                                                                     ########
+//#####################################################################################
+
+
+function returnLineFromMyCode(position) {
+    var fileWithMyScript = new File($.fileName) ;
+    var lineOfCode ;
+    try {
+        fileWithMyScript.open('r') ;
+        fileWithMyScript.seek(position) ;
+        lineOfCode = fileWithMyScript.readln() ;
+    } catch(someError) {
+        alert(someError) ;
+    } finally {
+        try {
+            fileWithMyScript.close();
+        } catch(someError) {}
+    }
+    return lineOfCode ;
+}
+
+
+function buldArrayOfNumAndString(codeLinesOfJSResource){
+    codeLinesOfJSResource = codeLinesOfJSResource || 6;
+    var arrOfNumAndString = [];
+
+    var i=1;
+    var strOfCode ="";
+    while (arrOfNumAndString.length < codeLinesOfJSResource){
+        strOfCodeNext = returnLineFromMyCode(i) || "";
+        if (strOfCodeNext.length>strOfCode.length){
+            var arrOfNumAndStringElement = [i, "\"" +strOfCodeNext + "\""];
+            arrOfNumAndString.push(arrOfNumAndStringElement);
+        }
+        strOfCode = strOfCodeNext;
+        i++;
+    }
+    var result = "\r    var jsResourceArray = [\r";
+    for (i=0; i< codeLinesOfJSResource; i++){
+        result += "        [" + arrOfNumAndString[i] + "],\r"
+    }
+    result += "    ];";
+    return result;
+}
+
+buldArrayOfNumAndString(); // paste quantity of lines with <javascriptresource>
---
+++ jsResourseDefenceChecker.jsx
@@ -0,0 +1,69 @@
+﻿<javascriptresource>
+<name>scriptName</name>
+<menu>filter</menu>
+<about>info about your script</about>
+<category>scriptCathegory</category>
+</javascriptresource>
+
+//##################################################################################################################//
+//########                                                                                                  ########//
+//########                          Copy next part of code to your script                                   ########//
+//########                                                                                                  ########//
+//##################################################################################################################//
+
+
+function ifJSResourseNotModify(){
+    
+    //#####################################################################################//
+    //########                                                                     ########//
+    //########     Build this array with script "jsResourseDefenceBuilder.jsx"     ########//
+    //########         Run it in ExtendScript Tool Kit                             ########//
+    //########         & copy it from ExtendScript "Javascript console"            ########//
+    //########                                                                     ########//
+    //########     This is an example of <javascriptresource>                      ########//
+    //########                                                                     ########//
+    //########     If you want to check correctness, paste your                    ########//
+    //########         <javascriptresource> to the begin of this file              ########//
+    //########         & run this script in ESTK/                                  ########//
+    //########                                                                     ########//
+    //#####################################################################################//
+
+    var jsResourceArray = [
+        [3,"<javascriptresource>"],
+        [25,"<name>scriptName</name>"],
+        [50,"<menu>filter</menu>"],
+        [71,"<about>info about your script</about>"],
+        [110,"<category>scriptCathegory</category>"],
+        [148,"</javascriptresource>"],
+    ];
+
+    for (var i=0; i<jsResourceArray.length; i++){
+        if (returnLineFromMyCode(jsResourceArray[i][0]) !== jsResourceArray[i][1]) {return false}
+    }
+    return true;
+
+    function returnLineFromMyCode(position) {
+        var fileWithMyScript = new File($.fileName) ;
+        var lineOfCode ;
+        try {
+            fileWithMyScript.open('r') ;
+            fileWithMyScript.seek(position) ;
+            lineOfCode = fileWithMyScript.readln() ;
+        } catch(someError) {
+            alert(someError) ;
+        } finally {
+            try {
+                fileWithMyScript.close();
+            } catch(someError) {}
+        }
+        return lineOfCode ;
+    }
+}
+
+//##################################################################################################################//
+//########                                                                                                  ########//
+//########                          End of part of code, which need to copy                                 ########//
+//########                                                                                                  ########//
+//##################################################################################################################//
+
+ifJSResourseNotModify(); //run this script & see Result in ESTK Javascript Console
